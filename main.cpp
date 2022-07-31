3805eddb92a44beee0e2af659174199bd9fea342
wtb2015 <wtb2015@gmail.com>
2022-07-31 21:03:40 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <adder.h>
+int main() 
+{ 
+    std::cout << "Hey, Zeus!\n"; 
+    std::cout << add(72.1 , 73.8) << '\n';
+    return 0; 
+}
---
+++ out/build/CMakeFiles/OLAS.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/wtb20/Desktop/cmake_tutorials/OurLordandSavior/main.cpp" "/mnt/c/Users/wtb20/Desktop/cmake_tutorials/OurLordandSavior/out/build/CMakeFiles/OLAS.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../Adder"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/c/Users/wtb20/Desktop/cmake_tutorials/OurLordandSavior/out/build/Adder/CMakeFiles/adder.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
